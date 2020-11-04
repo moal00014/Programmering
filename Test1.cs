19510f6e883cd04799d1b418c170feb9a77a410e
moal00014 <70176955+moal00014@users.noreply.github.com>
2020-11-04 15:20:15 +0100

    Test1
    Jag testade länken så att den fungerar!!

---
+++ Test1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 5;
+            int tal2 = 3;
+            Console.WriteLine("välj räknesätt!");
+            Console.WriteLine("1: Addition ");
+            Console.WriteLine("2: Subtraktion ");
+            Console.WriteLine("3: Division ");
+            Console.WriteLine("4: Multiplikation ");
+            int val = int.Parse(Console.ReadLine());
+            int resultat = 0;
+
+            if (val == 1)
+                resultat = tal1 + tal2;
+
+            else if (val == 2)
+
+                resultat = tal1 - tal2;
+            else if (val == 3)
+
+                resultat = tal1 / tal2;
+            else if (val == 4)
+
+                resultat = tal1 * tal2;
+
+
+            Console.Write("Resultatet av beräkningar är: " + resultat);
+
+
+
+        }
+    }
+}
